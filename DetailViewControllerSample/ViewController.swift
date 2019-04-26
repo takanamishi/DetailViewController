3834ddae45ea7ae6efc1654da62390088be1e0db
takanamishi <namikata@actindi.net>
2019-04-26 10:36:25 +0900

    first commit

---
+++ DetailViewControllerSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetailViewControllerSample
+//
+//  Created by 波形孝 on 2019/04/26.
+//  Copyright © 2019 template. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
